bad2c4c2da2b6b7512a19d0a5d458e494f247bc1
Shi <www.shiqi.com@gmail.com>
2022-09-02 21:34:45 +0800

    更新了一些模块。

---
+++ QsPlus.Framework/Fsm/IFsmManager.cs
@@ -0,0 +1,18 @@
+//------------------------------------------------------------
+// QsPlus.Framework ??? ...最垃圾的框架...Fuck..!
+// Copyright © 2022-2035 Shi Qi. All rights reserved.
+// GitHub : https://github.com/ShiQi2022/QsPlus.Framework
+// E-mail : www.shiqi.com@gmail.com
+// QQ : 2581424471@qq.com
+//------------------------------------------------------------
+
+namespace QsPlus.Framework.Fsm
+{
+    /// <summary>
+    /// 状态机管理器接口。
+    /// </summary>
+    public interface IFsmManager
+    {
+        
+    }
+}
