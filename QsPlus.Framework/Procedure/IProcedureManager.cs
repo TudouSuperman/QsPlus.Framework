1e260ab645347bb4e096f56aac5924fbabf3a5c2
Shi <www.shiqi.com@gmail.com>
2022-09-20 21:44:51 +0800

    更新。

---
+++ QsPlus.Framework/Procedure/IProcedureManager.cs
@@ -0,0 +1,14 @@
+//------------------------------------------------------------
+// QsPlus.Framework ??? ...最垃圾的框架...Fuck..!
+// Copyright © 2022-2035 Shi Qi. All rights reserved.
+// GitHub : https://github.com/ShiQi2022/QsPlus.Framework
+// E-mail : www.shiqi.com@gmail.com
+//------------------------------------------------------------
+
+namespace QsPlus.Framework.Procedure
+{
+    public interface IProcedureManager
+    {
+        
+    }
+}
